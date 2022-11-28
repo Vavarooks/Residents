bd1d67e59d995a5a3722c02d0efaaaf460f6028e
Vanessa Emerick <vavarooks123@gmail.com>
2022-11-28 14:07:50 -0800

    working on adding images

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Routes, Route } from 'react-router-dom';
+import Home from './views/Home'
+import Contact from './views/Contact'
+import About from './views/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/about'}>About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/contact'}>Contact Us</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -3,7 +3,48 @@ import React from "react";
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <h1>Welcome</h1>
+            <img src={process.env.PUBLIC_URL + './img/front-door.jpeg'} class="img-fluid" alt="..."/>
+            <div className='container d-block mx-auto w-50 my-3 p-3' >
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
